9555d670484b16da8dfde356c16861500f0c5bd8
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-02-26 11:49:02 +0100

    Added basic form handling and added the typewriter js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert ("Hello world!")
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: " Jewel in the sky Lit me up As you're lit up In the sky The glow in",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
